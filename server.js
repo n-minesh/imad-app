fe93709860c6cb3e5731fe9e94d19f51240c4592
n-minesh <31245823+n-minesh@users.noreply.github.com>
2017-09-02 17:57:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article-one', function(req,res){
 app.get('/article-two', function(req,res){
    res.send("Article two is requested and will be served") ;
 });
-app.get('/article-three', function(req,res){
+app.get('/article three', function(req,res){
    res.send("Article three is requested and will be served") ;
 });
 
