1b98d64e78522eab5fcb69e4759733ff5881c8f5
n-minesh <31245823+n-minesh@users.noreply.github.com>
2017-09-03 14:46:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/:articleName', function(req,res){
     res.send(createTemplate (articles[articleName]));
 });
 
-app.get('/main.js', function(req,res){
+app.get('/ui/main.js', function(req,res){
  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
