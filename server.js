2be69c4ad6f7fb8033040b7ac58d10717491fcda
n-minesh <31245823+n-minesh@users.noreply.github.com>
2017-09-03 13:38:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var articleOne = {
                 </p>`
     };
     
-function createTemplate(data){
+function createTemplate (data){
     
       var title= 'data.title';
       var date= 'data.date';
