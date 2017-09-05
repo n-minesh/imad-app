50b2d82aeca2c1c5e5ddd0cfa9e9e564406a9b19
n-minesh <31245823+n-minesh@users.noreply.github.com>
2017-09-05 20:23:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var coumter=0;
+var counter = 0;
 app.get('/counter', function (req, res) {
    counter = counter + 1;
    res.send(counter.toString());
