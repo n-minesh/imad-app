f106725a59ca808d3f7e66061c38e31c9738ad2f
n-minesh <31245823+n-minesh@users.noreply.github.com>
2017-09-05 20:07:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var img = document.getElementById('mad');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft + 1;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
